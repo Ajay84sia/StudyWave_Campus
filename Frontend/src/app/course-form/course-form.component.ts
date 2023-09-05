b722f29bf1f795f422486969a6a94859ee02d2b0
Ajay84sia <ajay9679339@gmail.com>
2023-09-05 12:09:35 +0530

    assignment done

--- Frontend/src/app/course-form/course-form.component.ts
+++ Frontend/src/app/course-form/course-form.component.ts
@@ -1,32 +1,40 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {CourseService } from '../course.service';
+import { CourseService } from '../course.service';
 import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
-
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.css']
+  styleUrls: ['./course-form.component.css'],
 })
 export class CourseFormComponent {
   course: any = {
     CourseCode: '',
     Name: '',
-    DepartmentID:'',
-    Credits: null,
+    DepartmentID: '',
+    Credits: '',
     Description: '',
-    InstructorID:''
+    InstructorID: '',
   };
 
-  constructor(private courseService: CourseService, private router : Router, private toast : NgToastService) {}
+  constructor(
+    private courseService: CourseService,
+    private router: Router,
+    private toast: NgToastService
+  ) {}
 
   onSubmit(): void {
-    this.courseService.postCourse(this.course).subscribe(response => {
+    this.courseService.postCourse(this.course).subscribe((response) => {
       console.log('Course added:', response);
       this.course = {};
-      this.toast.success({detail:"SUCCESS",summary:'New Course added Successfully',duration:2000, position:'botomCenter'});
+      this.toast.success({
+        detail: 'SUCCESS',
+        summary: 'New Course added Successfully',
+        duration: 2000,
+        position: 'botomCenter',
+      });
       this.router.navigate(['/course']);
     });
   }
